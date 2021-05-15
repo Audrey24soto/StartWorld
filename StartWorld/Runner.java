119e4c512bda10c417ec167301e102ea80c4132b
Adriana Gillispie <83720329+Audrey24soto@users.noreply.github.com>
2021-05-14 21:17:15 -0500

    Add files via upload

---
+++ StartWorld/Runner.java
@@ -0,0 +1,14 @@
+package com.qa.StartWorld;
+
+// Class has a scope
+public class Runner {
+	
+	public static void main(String[] args) {
+	
+		
+		int result = 5 / 5;
+		
+		System.out.println(result);
+	}
+
+}
